263df823b97b9fdadfee0d7b9129d31fdf41c789
gillianbc <gszemeti@gmail.com>
2023-01-23 21:53:56 +0000

    Stop ship moving off left and right of containing screen

--- ship.py
+++ ship.py
@@ -4,6 +4,8 @@ class Ship:
 
     def __init__(self, ai_game):
         """ Initialise the ship and set its starting position """
+        # Not sure why we're making copies of ai_game's properties within a ship
+        # rather than just accessing them from ship.ai_game as and when...
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -24,9 +26,9 @@ class Ship:
 
     def update(self):
         """ Update the ship's x value based on the movement flag and the speed """
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         # Update the rect object of the ship based on its x value
         self.rect.x = self.x
